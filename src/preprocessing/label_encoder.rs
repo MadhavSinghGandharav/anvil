192bc43dfa82bd6b3432bd6e6ebec20fd850df52
MadhavSinghGandharav <msg200415@gmail.com>
2026-03-08 23:27:42 +0530

    fixed LabelEncoder

--- src/preprocessing/label_encoder.rs
+++ src/preprocessing/label_encoder.rs
@@ -1,39 +1,55 @@
-use std::collections::HashMap;
+use std::collections::{HashMap,hash_map::Entry};
 use std::hash::Hash;
+use std::usize;
+use std::fmt::Debug;
 
 pub struct LabelEncoder<T> {
-    classes: Vec<T>,
+    pub classes: Vec<T>,
+    map: HashMap<T, usize>
 }
+
+
 
 impl<T> LabelEncoder<T>
 where
-    T: Eq + Hash + Clone,
+    T: Eq + Hash + Clone + Debug,
 {
     pub fn new() -> Self {
-        Self { classes: Vec::new() }
+        Self { 
+            classes: Vec::new(),
+            map: HashMap::new()
+        }
+    }
+    
+    pub fn classes(&self) -> &Vec<T>{
+        &self.classes
+    }
+    pub fn fit(&mut self, y: &[T]) {
+        for label in y {
+            match self.map.entry(label.clone()) {
+                Entry::Occupied(_) => {}
+                Entry::Vacant(v) => {
+                    let id = self.classes.len();
+                    v.insert(id);
+                    self.classes.push(label.clone());
+                }
+            }
+        }
     }
 
-    pub fn fit_transform(&mut self, y: &[T]) -> Vec<usize> {
-        let mut map: HashMap<T, usize> = HashMap::new();
+    pub fn transform(&self, y:&[T]) -> Vec<usize> {
         let mut encoded = Vec::with_capacity(y.len());
 
-        for label in y {
-            let idx = match map.get(label) {
-                Some(&i) => i,
-                None => {
-                    let new_idx = map.len();
-                    map.insert(label.clone(), new_idx);
-                    self.classes.push(label.clone());
-                    new_idx
-                }
-            };
-            encoded.push(idx);
+        for label in y{
+            match self.map.get(label) {
+                Some(&id) => encoded.push(id),
+                None => panic!("new values encounterd {:?}",label),
+            }
         }
-
         encoded
     }
-
-    pub fn classes(&self) -> &[T] {
-        &self.classes
+    pub fn fit_transform(&mut self, y: &[T]) -> Vec<usize> {
+        self.fit(y);
+        self.transform(y)
     }
 }
